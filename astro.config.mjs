99157fd7ac7a9adc53a380eec47af68251005a10
lowestofthe1ow <1owestoftheow@gmail.com>
2026-06-03 16:27:16 +0800

    Set up layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import icon from 'astro-icon';
 
 export default defineConfig({
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), icon()],
 });
+
+
