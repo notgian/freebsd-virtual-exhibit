806b223b748ea18adb27d73085e7a30243b7a281
Gian <85060753+notgian@users.noreply.github.com>
2026-07-07 22:06:31 +0800

    chore: attempting to push to trigger gh workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   integrations: [mdx(), react(), icon()],
   site: 'https://notgian.github.io',
-  base: 'virtual-exhibit-template',
+  base: 'virtual-exhibit',
 
   vite: {
     plugins: [tailwindcss()],
