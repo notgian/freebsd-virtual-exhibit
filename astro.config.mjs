ed7848e19c414de9cbe39f8edb3cc2219b1d5435
Gian <85060753+notgian@users.noreply.github.com>
2026-07-07 22:01:59 +0800

    chore: changed name in astro config for gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   integrations: [mdx(), react(), icon()],
-  site: 'https://jrgo7.github.io',
+  site: 'https://notgian.github.io',
   base: 'virtual-exhibit-template',
 
   vite: {
